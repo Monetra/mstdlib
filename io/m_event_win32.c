722f6461436c9bed218a0b9712ae4fb12162e5a3
Brad House <brad@monetra.com>
2017-09-20 19:54:38 -0400

    [win32] don't ever deliver ERROR or DISCONNECT

--- io/m_event_win32.c
+++ io/m_event_win32.c
@@ -595,12 +595,13 @@ static void M_event_impl_win32_process(M_event_t *event)
 			 * could use the value as last_error_sys */
 			M_event_deliver_io(event, member->io, M_EVENT_TYPE_READ);
 
-			/* Enqueue a softevent for a disconnect (or READ for ERROR) as otherwise
+			/* Enqueue a softevent for a READ on a disconnect or ERROR as otherwise
 			 * it will do a partial read if there is still data buffered, and not ever attempt
 			 * to read again.   We do this as a soft event as it is delivered after processing
-			 * of normal events.  We tried using M_event_deliver_io() again instead and it didn't
-			 * work ... at least for blocking i/o */
-			M_io_softevent_add(member->io, 0, (NetEvents.iErrorCode[FD_CLOSE_BIT] == 0)?M_EVENT_TYPE_DISCONNECTED:M_EVENT_TYPE_READ);
+			 * of normal events.  Do NOT try to enqueue a DISCONNECT or ERROR as it might wipe
+			 * pending events on io_net_dns and never actually push up the read event so it
+			 * gets used. */
+			M_io_softevent_add(member->io, 0, M_EVENT_TYPE_READ);
 		}
 
 		/* Treat CONNECT and WRITE events as WRITE events */
