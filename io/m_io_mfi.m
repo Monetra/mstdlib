f59ccfb2270352581cb81221415a8c793e4ade75
John Schember <john@nachtimwald.com>
2017-12-13 17:24:51 -0500

    Move bluetooth ios to mfi since it is the made for i* protocol not standard bluetooth.

--- io/m_io_mfi.m
+++ io/m_io_mfi.m
@@ -22,17 +22,52 @@
  */
 
 #include <mstdlib/mstdlib.h>
-#include <mstdlib/io/m_io_bluetooth.h>
-#include "m_io_bluetooth_int.h"
-#include "m_io_bluetooth_ios.h"
+#include <mstdlib/io/m_io_mfi.h>
+#include "m_io_mfi_int.h"
+#include "m_io_mfi_ios.h"
 #import <ExternalAccessory/ExternalAccessory.h>
 
+#define M_IO_MFI_NAME "MFI"
 
-M_io_bluetooth_enum_t *M_io_bluetooth_enum(void)
+typedef struct {
+	char *name;
+	char *protocol;
+	char *serialnum
+} M_io_mfi_enum_device_t;
+
+struct M_io_mfi_enum {
+	M_list_t *devices; /* list of M_io_mfi_enum_device_t */
+};
+
+static void M_io_mfi_enum_free_device(void *arg)
+{
+	M_io_mfi_enum_device_t *device = arg;
+	M_free(device->name);
+	M_free(device->protocol);
+	M_free(device->serialnum);
+	M_free(device);
+}
+
+static void M_io_mfi_enum_add(M_io_mfi_enum_t *mfienum, const char *name, const char *protocol, const char *serialnum)
+{
+	M_io_mfi_enum_device_t *device;
+
+	if (mfienum == NULL || M_str_isempty(name) || M_str_isempty(protocol) || M_str_isempty(serialnum))
+		return;
+
+	device            = M_malloc_zero(sizeof(*device));
+	device->name      = M_strdup(name);
+	device->protocol  = M_strdup(protocol);
+	device->serialnum = M_strdup(serialnum);
+
+	M_list_insert(mfienum->devices, device);
+}
+
+M_io_mfi_enum_t *M_io_mfi_enum(void)
 {
 	NSArray<EAAccessory *> *accs;
 	EAAccessory            *acc;
-	M_io_bluetooth_enum_t *btenum = M_io_bluetooth_enum_init();
+	M_io_mfi_enum_t *mfienum = M_io_mfi_enum_init();
 
 	accs = [[EAAccessoryManager sharedAccessoryManager] connectedAccessories];
 	for (acc in accs) {
@@ -40,29 +75,90 @@ M_io_bluetooth_enum_t *M_io_bluetooth_enum(void)
 			const char *name      = [acc.name UTF8String];
 			const char *serialnum = [acc.serialNumber UTF8String];
 			const char *protocol  = [k UTF8String];
-			M_io_bluetooth_enum_add(btenum, name, protocol, serialnum);
+			M_io_mfi_enum_add(mfienum, name, protocol, serialnum);
 		}
 	}
 
-	return btenum;
+	return mfienum;
+}
+
+void M_io_mfi_enum_destroy(M_io_mfi_enum_t *mfienum)
+{
+	if (mfienum == NULL)
+		return;
+	M_list_destroy(mfienum->devices, M_TRUE);
+	M_free(mfienum);
+}
+
+M_io_mfi_enum_t *M_io_mfi_enum_init(void)
+{
+	M_io_mfi_enum_t  *mfienum  = M_malloc_zero(sizeof(*mfienum));
+	struct M_list_callbacks listcbs = {
+		NULL,
+		NULL,
+		NULL,
+		M_io_mfi_enum_free_device
+	};
+	mfienum->devices = M_list_create(&listcbs, M_LIST_NONE);
+	return mfienum;
+}
+
+size_t M_io_mfi_enum_count(const M_io_mfi_enum_t *mfienum)
+{
+	if (mfienum == NULL)
+		return 0;
+	return M_list_len(mfienum->devices);
 }
 
 
-M_io_handle_t *M_io_bluetooth_open(const char *mac, const char *uuid, M_io_error_t *ioerr)
+const char *M_io_mfi_enum_name(const M_io_mfi_enum_t *mfienum, size_t idx)
 {
-	M_io_handle_t         *handle     = NULL;
-	M_io_bluetooth_ios_ea *ea         = nil;
-	NSString              *myuuid     = nil;
+	const M_io_mfi_enum_device_t *device;
+	if (mfienum == NULL)
+		return NULL;
+	device = M_list_at(mfienum->devices, idx);
+	if (device == NULL)
+		return NULL;
+	return device->name;
+}
+
+const char *M_io_mfi_enum_protocol(const M_io_mfi_enum_t *mfienum, size_t idx)
+{
+	const M_io_mfi_enum_device_t *device;
+	if (mfienum == NULL)
+		return NULL;
+	device = M_list_at(mfienum->devices, idx);
+	if (device == NULL)
+		return NULL;
+	return device->protocol;
+}
+
+const char *M_io_mfi_enum_serialnum(const M_io_mfi_enum_t *mfienum, size_t idx)
+{
+	const M_io_mfi_enum_device_t *device;
+	if (mfienum == NULL)
+		return NULL;
+	device = M_list_at(mfienum->devices, idx);
+	if (device == NULL)
+		return NULL;
+	return device->serialnum;
+}
+
+static M_io_handle_t *M_io_mfi_open(const char *protocol, const char *serialnum, M_io_error_t *ioerr)
+{
+	M_io_handle_t   *handle      = NULL;
+	M_io_mfi_ios_ea *ea          = nil;
+	NSString        *myserialnum = nil;
 
 	*ioerr = M_IO_ERROR_SUCCESS;
 
-	if (mac == NULL) {
+	if (protocol == NULL) {
 		*ioerr = M_IO_ERROR_INVALID;
 		return NULL;
 	}
 
-	if (uuid != NULL)
-		myuuid = [NSString stringWithUTF8String:uuid];
+	if (serialnum != NULL)
+		myserialnum = [NSString stringWithUTF8String:serialnum];
 
 	/* All pre-validations are good here.  We're not going to start the actual connection yet as that
 	 * is a blocking operation.  All of the above should have been non-blocking. */
@@ -71,7 +167,7 @@ M_io_handle_t *M_io_bluetooth_open(const char *mac, const char *uuid, M_io_error
 	handle->writebuf  = M_buf_create();
 	M_snprintf(handle->error, sizeof(handle->error), "Error not set");
 
-	ea = [M_io_bluetooth_ios_ea m_io_bluetooth_ios_ea:[NSString stringWithUTF8String:mac] handle:handle serialnum:myuuid];
+	ea = [M_io_mfi_ios_ea m_io_mfi_ios_ea:[NSString stringWithUTF8String:protocol] handle:handle serialnum:myserialnum];
 	if (ea == nil) {
 		*ioerr = M_IO_ERROR_NOTFOUND;
 		M_buf_cancel(handle->readbuf);
@@ -85,8 +181,7 @@ M_io_handle_t *M_io_bluetooth_open(const char *mac, const char *uuid, M_io_error
 	return handle;
 }
 
-
-M_bool M_io_bluetooth_errormsg_cb(M_io_layer_t *layer, char *error, size_t err_len)
+static M_bool M_io_mfi_errormsg_cb(M_io_layer_t *layer, char *error, size_t err_len)
 {
 	M_io_handle_t *handle = M_io_layer_get_handle(layer);
 	if (M_str_isempty(handle->error))
@@ -96,29 +191,26 @@ M_bool M_io_bluetooth_errormsg_cb(M_io_layer_t *layer, char *error, size_t err_l
 	return M_TRUE;
 }
 
-
-M_io_state_t M_io_bluetooth_state_cb(M_io_layer_t *layer)
+static M_io_state_t M_io_mfi_state_cb(M_io_layer_t *layer)
 {
 	M_io_handle_t *handle = M_io_layer_get_handle(layer);
 	return handle->state;
 }
 
-
-static void M_io_bluetooth_close(M_io_handle_t *handle, M_io_state_t state)
+static void M_io_mfi_close(M_io_handle_t *handle, M_io_state_t state)
 {
-	M_io_bluetooth_ios_ea *ea;
+	M_io_mfi_ios_ea *ea;
 
 	if (handle->ea != nil) {
 		handle->state = state;
-		ea = (__bridge_transfer M_io_bluetooth_ios_ea *)handle->ea;
+		ea = (__bridge_transfer M_io_mfi_ios_ea *)handle->ea;
 		[ea close];
 		ea            = nil;
 		handle->ea    = nil;
 	}
 }
 
-
-void M_io_bluetooth_destroy_cb(M_io_layer_t *layer)
+static void M_io_mfi_destroy_cb(M_io_layer_t *layer)
 {
 	M_io_handle_t *handle = M_io_layer_get_handle(layer);
 
@@ -127,15 +219,14 @@ void M_io_bluetooth_destroy_cb(M_io_layer_t *layer)
 		handle->timer = NULL;
 	}
 
-	M_io_bluetooth_close(handle, M_IO_STATE_DISCONNECTED);
+	M_io_mfi_close(handle, M_IO_STATE_DISCONNECTED);
 
 	M_buf_cancel(handle->readbuf);
 	M_buf_cancel(handle->writebuf);
 	M_free(handle);
 }
 
-
-M_bool M_io_bluetooth_process_cb(M_io_layer_t *layer, M_event_type_t *type)
+static M_bool M_io_mfi_process_cb(M_io_layer_t *layer, M_event_type_t *type)
 {
 	M_io_handle_t *handle = M_io_layer_get_handle(layer);
 	(void)layer;
@@ -151,8 +242,7 @@ M_bool M_io_bluetooth_process_cb(M_io_layer_t *layer, M_event_type_t *type)
 	return M_FALSE;
 }
 
-
-M_io_error_t M_io_bluetooth_write_cb(M_io_layer_t *layer, const unsigned char *buf, size_t *write_len)
+static M_io_error_t M_io_mfi_write_cb(M_io_layer_t *layer, const unsigned char *buf, size_t *write_len)
 {
 	M_io_handle_t *handle   = M_io_layer_get_handle(layer);
 
@@ -166,14 +256,13 @@ M_io_error_t M_io_bluetooth_write_cb(M_io_layer_t *layer, const unsigned char *b
 
 	/* Dispatch an attempt to write if there was no previously enqueued data. */
 	if (M_buf_len(handle->writebuf) == *write_len) {
-		[(__bridge M_io_bluetooth_ios_ea *)handle->ea write_data_buffered];
+		[(__bridge M_io_mfi_ios_ea *)handle->ea write_data_buffered];
 	}
 
 	return M_IO_ERROR_SUCCESS;
 }
 
-
-M_io_error_t M_io_bluetooth_read_cb(M_io_layer_t *layer, unsigned char *buf, size_t *read_len)
+static M_io_error_t M_io_mfi_read_cb(M_io_layer_t *layer, unsigned char *buf, size_t *read_len)
 {
 	M_io_handle_t *handle = M_io_layer_get_handle(layer);
 
@@ -194,9 +283,7 @@ M_io_error_t M_io_bluetooth_read_cb(M_io_layer_t *layer, unsigned char *buf, siz
 	return M_IO_ERROR_SUCCESS;
 }
 
-
-
-static void M_io_bluetooth_timer_cb(M_event_t *event, M_event_type_t type, M_io_t *dummy_io, void *arg)
+static void M_io_mfi_timer_cb(M_event_t *event, M_event_type_t type, M_io_t *dummy_io, void *arg)
 {
 	M_io_handle_t *handle = arg;
 	M_io_layer_t  *layer;
@@ -212,12 +299,12 @@ static void M_io_bluetooth_timer_cb(M_event_t *event, M_event_type_t type, M_io_
 	if (handle->state == M_IO_STATE_CONNECTING) {
 
 		/* Tell the thread to shutdown by closing the socket on our end */
-		M_io_bluetooth_close(handle, M_IO_STATE_ERROR);
+		M_io_mfi_close(handle, M_IO_STATE_ERROR);
 
 		M_snprintf(handle->error, sizeof(handle->error), "Timeout waiting on connect");
 		M_io_layer_softevent_add(layer, M_TRUE, M_EVENT_TYPE_ERROR);
 	} else if (handle->state == M_IO_STATE_DISCONNECTING) {
-		M_io_bluetooth_close(handle, M_IO_STATE_DISCONNECTED);
+		M_io_mfi_close(handle, M_IO_STATE_DISCONNECTED);
 		M_io_layer_softevent_add(layer, M_TRUE, M_EVENT_TYPE_DISCONNECTED);
 	} else {
 		/* Shouldn't ever happen */
@@ -226,7 +313,7 @@ static void M_io_bluetooth_timer_cb(M_event_t *event, M_event_type_t type, M_io_
 	M_io_layer_release(layer);
 }
 
-M_bool M_io_bluetooth_disconnect_cb(M_io_layer_t *layer)
+static M_bool M_io_mfi_disconnect_cb(M_io_layer_t *layer)
 {
 	M_io_handle_t *handle = M_io_layer_get_handle(layer);
 
@@ -235,17 +322,16 @@ M_bool M_io_bluetooth_disconnect_cb(M_io_layer_t *layer)
 
 	if (M_buf_len(handle->writebuf)) {
 		/* Data pending, delay 100ms */
-		handle->timer = M_event_timer_oneshot(M_io_get_event(M_io_layer_get_io(layer)), 100, M_TRUE, M_io_bluetooth_timer_cb, handle);
+		handle->timer = M_event_timer_oneshot(M_io_get_event(M_io_layer_get_io(layer)), 100, M_TRUE, M_io_mfi_timer_cb, handle);
 		handle->state = M_IO_STATE_DISCONNECTING;
 		return M_FALSE;
 	}
 
-	M_io_bluetooth_close(handle, M_IO_STATE_DISCONNECTED);
+	M_io_mfi_close(handle, M_IO_STATE_DISCONNECTED);
 	return M_TRUE;
 }
 
-
-void M_io_bluetooth_unregister_cb(M_io_layer_t *layer)
+static void M_io_mfi_unregister_cb(M_io_layer_t *layer)
 {
 	M_io_handle_t *handle = M_io_layer_get_handle(layer);
 
@@ -257,8 +343,7 @@ void M_io_bluetooth_unregister_cb(M_io_layer_t *layer)
 
 }
 
-
-M_bool M_io_bluetooth_init_cb(M_io_layer_t *layer)
+static M_bool M_io_mfi_init_cb(M_io_layer_t *layer)
 {
 	M_io_handle_t   *handle = M_io_layer_get_handle(layer);
 	M_io_t          *io     = M_io_layer_get_io(layer);
@@ -268,12 +353,12 @@ M_bool M_io_bluetooth_init_cb(M_io_layer_t *layer)
 		case M_IO_STATE_INIT:
 			handle->state = M_IO_STATE_CONNECTING;
 			handle->io    = io;
-			[(__bridge M_io_bluetooth_ios_ea *)handle->ea connect];
+			[(__bridge M_io_mfi_ios_ea *)handle->ea connect];
 
 			/* Fall-thru */
 		case M_IO_STATE_CONNECTING:
 			/* start timer to time out operation */
-			handle->timer = M_event_timer_oneshot(event, 10000, M_TRUE, M_io_bluetooth_timer_cb, handle);
+			handle->timer = M_event_timer_oneshot(event, 10000, M_TRUE, M_io_mfi_timer_cb, handle);
 			break;
 		case M_IO_STATE_CONNECTED:
 			/* Trigger connected soft event when registered with event handle */
@@ -292,3 +377,32 @@ M_bool M_io_bluetooth_init_cb(M_io_layer_t *layer)
 	return M_TRUE;
 }
 
+M_io_error_t M_io_mfi_create(M_io_t **io_out, const char *protocol, const char *serialnum)
+{
+	M_io_handle_t    *handle;
+	M_io_callbacks_t *callbacks;
+	M_io_error_t      err;
+
+	if (io_out == NULL || M_str_isempty(protocol))
+		return M_IO_ERROR_INVALID;
+
+	handle    = M_io_mfi_open(protocol, serialnum, &err);
+	if (handle == NULL)
+		return err;
+
+	*io_out   = M_io_init(M_IO_TYPE_STREAM);
+	callbacks = M_io_callbacks_create();
+	M_io_callbacks_reg_init(callbacks, M_io_mfi_init_cb);
+	M_io_callbacks_reg_read(callbacks, M_io_mfi_read_cb);
+	M_io_callbacks_reg_write(callbacks, M_io_mfi_write_cb);
+	M_io_callbacks_reg_processevent(callbacks, M_io_mfi_process_cb);
+	M_io_callbacks_reg_unregister(callbacks, M_io_mfi_unregister_cb);
+	M_io_callbacks_reg_destroy(callbacks, M_io_mfi_destroy_cb);
+	M_io_callbacks_reg_disconnect(callbacks, M_io_mfi_disconnect_cb);
+	M_io_callbacks_reg_state(callbacks, M_io_mfi_state_cb);
+	M_io_callbacks_reg_errormsg(callbacks, M_io_mfi_errormsg_cb);
+	M_io_layer_add(*io_out, M_IO_MFI_NAME, handle, callbacks);
+	M_io_callbacks_destroy(callbacks);
+
+	return M_IO_ERROR_SUCCESS;
+}
