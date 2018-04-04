3b0b35084069a46e8bae843148c92ab0e8fe6b1a
John Schember <john@nachtimwald.com>
2018-04-04 11:47:41 -0400

    check event timer: Add test for stopping timers.

--- test/io/check_event_interactions.c
+++ test/io/check_event_interactions.c
@@ -76,6 +76,28 @@ static void el_remove_cb2(M_event_t *el, M_event_type_t etype, M_io_t *io, void 
 	M_event_done(el1);
 }
 
+static void el_stop_cb2(M_event_t *el, M_event_type_t etype, M_io_t *io, void *thunk)
+{
+	(void)el;
+	(void)etype;
+	(void)io;
+	(void)thunk;
+
+	/* Try to run start a bunch of times on the same event timer */
+	for (size_t i=0; i<25; i++) {
+		M_event_timer_stop(timer1);
+		M_event_timer_start(timer1, 0);
+
+		/* Sleep enough to yield execution for each in case more timers go off */
+		M_thread_sleep(15000);
+	}
+
+	M_event_done(el2);
+	/* Sleep long enough that we know the thread1 callback is complete */
+	M_thread_sleep(2000000);
+	M_event_done(el1);
+}
+
 static void *run_el2(void *arg)
 {
 	(void)arg;
@@ -96,7 +118,7 @@ static void *run_el1(void *arg)
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-START_TEST(check_event_interactions)
+START_TEST(check_event_stacking_start)
 {
 	M_thread_attr_t *tattr;
 	M_threadid_t     t1;
@@ -158,6 +180,37 @@ START_TEST(check_event_remove)
 }
 END_TEST
 
+START_TEST(check_event_stop)
+{
+	M_thread_attr_t *tattr;
+	M_threadid_t     t1;
+	M_threadid_t     t2;
+
+	el1    = M_event_create(M_EVENT_FLAG_NONE);
+	timer1 = M_event_timer_add(el1, el_cb, NULL);
+	M_event_timer_set_firecount(timer1, 1);
+
+	el2    = M_event_create(M_EVENT_FLAG_NONE);
+	timer2 = M_event_timer_add(el2, el_stop_cb2, NULL);
+	M_event_timer_set_firecount(timer2, 1);
+	M_event_timer_start(timer2, 0);
+
+	tattr = M_thread_attr_create();
+	M_thread_attr_set_create_joinable(tattr, M_TRUE);
+	t1 = M_thread_create(tattr, run_el1, NULL);
+	/* Give up time slice to make sure thread 1 is fully initialized */
+	M_thread_sleep(100000);
+
+	t2 = M_thread_create(tattr, run_el2, NULL);
+	M_thread_attr_destroy(tattr);
+
+	M_thread_join(t2, NULL);
+	M_thread_join(t1, NULL);
+
+	ck_assert_msg(count == 1, "Timer started by different thread fired unexpected number of times (%zu) expected (1)", count);
+}
+END_TEST
+
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 /* Checks the following:
@@ -177,13 +230,18 @@ static Suite *event_interactions_suite(void)
 
 	suite = suite_create("event_interactions");
 
-	tc = tcase_create("event_interactions");
-	tcase_add_test(tc, check_event_interactions);
+	tc = tcase_create("event_stacking_start");
+	tcase_add_test(tc, check_event_stacking_start);
 	tcase_set_timeout(tc, 60);
 	suite_add_tcase(suite, tc);
 
 	tc = tcase_create("event_remove");
 	tcase_add_test(tc, check_event_remove);
+	tcase_set_timeout(tc, 60);
+	suite_add_tcase(suite, tc);
+
+	tc = tcase_create("event_stop");
+	tcase_add_test(tc, check_event_stop);
 	tcase_set_timeout(tc, 60);
 	suite_add_tcase(suite, tc);
 
