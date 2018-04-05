80d79384a99c38513b19718bff60b0dc1eb3df5f
John Schember <john@nachtimwald.com>
2018-04-05 15:57:18 -0400

    check event interactions: add test for el queued with many timers.

--- test/io/check_event_interactions.c
+++ test/io/check_event_interactions.c
@@ -14,6 +14,7 @@ typedef struct {
 	M_event_timer_t *timer1;
 	M_event_timer_t *timer2;
 	size_t           count;
+	size_t           num;
 } cb_data_t;
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
@@ -51,6 +52,19 @@ static void el_self_cb(M_event_t *el, M_event_type_t etype, M_io_t *io, void *th
 		M_event_timer_remove(data->timer1);
 		M_event_done(data->el1);
 	}
+}
+
+static void el_many_cb(M_event_t *el, M_event_type_t etype, M_io_t *io, void *thunk)
+{
+	cb_data_t *data = thunk;
+
+	(void)el;
+	(void)etype;
+	(void)io;
+
+	data->count++;
+	if (data->count == data->num)
+		M_event_done(data->el1);
 }
 
 static void el_cb2(M_event_t *el, M_event_type_t etype, M_io_t *io, void *thunk)
@@ -137,6 +151,14 @@ static void *run_el1(void *arg)
 
 	M_event_loop(data->el1, M_TIMEOUT_INF);
 	M_event_destroy(data->el1);
+	return NULL;
+}
+
+static void *run_els(void *arg)
+{
+	cb_data_t *data = arg;
+
+	M_event_loop(data->el1, M_TIMEOUT_INF);
 	return NULL;
 }
 
@@ -268,6 +290,49 @@ START_TEST(check_event_self)
 }
 END_TEST
 
+START_TEST(check_event_many)
+{
+	M_thread_attr_t *tattr;
+	M_list_t        *l;
+	M_event_timer_t *timer;
+	M_threadid_t     t1;
+	size_t           i;
+	cb_data_t        data;
+	struct M_list_callbacks cbs = {
+		NULL,
+		NULL,
+		NULL,
+		(M_list_free_func)M_event_timer_remove
+	};
+
+
+	M_mem_set(&data, 0, sizeof(data));
+
+	data.num = 100000;
+	data.el1 = M_event_create(M_EVENT_FLAG_NONE);
+
+	l = M_list_create(&cbs, M_LIST_NONE);
+	for (i=0; i<data.num; i++) {
+		timer = M_event_timer_add(data.el1, el_many_cb, &data);
+		M_event_timer_set_firecount(timer, 1);
+		M_event_timer_start(timer, 0);
+		M_list_insert(l, timer);
+	}
+
+	tattr = M_thread_attr_create();
+	M_thread_attr_set_create_joinable(tattr, M_TRUE);
+	t1 = M_thread_create(tattr, run_els, &data);
+	M_thread_attr_destroy(tattr);
+
+	M_thread_join(t1, NULL);
+
+	M_list_destroy(l, M_TRUE);
+	M_event_destroy(data.el1);
+
+	ck_assert_msg(data.count == data.num, "Timer started by different thread fired unexpected number of times (%zu) expected (%zu)", data.count, data.num);
+}
+END_TEST
+
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 /* Checks the following:
@@ -311,6 +376,11 @@ static Suite *event_interactions_suite(void)
 
 	tc = tcase_create("event_self");
 	tcase_add_test(tc, check_event_self);
+	tcase_set_timeout(tc, 60);
+	suite_add_tcase(suite, tc);
+
+	tc = tcase_create("event_many");
+	tcase_add_test(tc, check_event_many);
 	tcase_set_timeout(tc, 60);
 	suite_add_tcase(suite, tc);
 
