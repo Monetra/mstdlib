4b756ee3a7c155ec3c46b19323267f8a3988883d
John Schember <john@nachtimwald.com>
2018-05-14 14:21:16 -0400

    textcodec: Add cp1250.

--- base/textcodec/m_textcodec_int.h
+++ base/textcodec/m_textcodec_int.h
@@ -69,6 +69,9 @@ M_textcodec_error_t M_textcodec_decode_ascii(M_textcodec_buffer_t *buf, const ch
 M_textcodec_error_t M_textcodec_encode_cp874(M_textcodec_buffer_t *buf, const char *in, M_textcodec_ehandler_t ehandler);
 M_textcodec_error_t M_textcodec_decode_cp874(M_textcodec_buffer_t *buf, const char *in, M_textcodec_ehandler_t ehandler);
 
+M_textcodec_error_t M_textcodec_encode_cp1250(M_textcodec_buffer_t *buf, const char *in, M_textcodec_ehandler_t ehandler);
+M_textcodec_error_t M_textcodec_decode_cp1250(M_textcodec_buffer_t *buf, const char *in, M_textcodec_ehandler_t ehandler);
+
 M_textcodec_error_t M_textcodec_encode_cp1252(M_textcodec_buffer_t *buf, const char *in, M_textcodec_ehandler_t ehandler);
 M_textcodec_error_t M_textcodec_decode_cp1252(M_textcodec_buffer_t *buf, const char *in, M_textcodec_ehandler_t ehandler);
 
