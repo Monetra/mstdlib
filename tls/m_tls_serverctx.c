04c8db8744195abc5b05aa28d95bc04ba1c7af78
John Schember <john@nachtimwald.com>
2019-03-08 11:26:06 -0500

    Update copyright with new compoany name. Main Street Softworks the compnay has been renamed to Monetra Technologies.

--- tls/m_tls_serverctx.c
+++ tls/m_tls_serverctx.c
@@ -1,6 +1,6 @@
 /* The MIT License (MIT)
  * 
- * Copyright (c) 2017 Main Street Softworks, Inc.
+ * Copyright (c) 2017 Monetra Technologies, LLC.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
