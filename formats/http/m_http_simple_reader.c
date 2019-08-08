c28f7c4fba3064784d43b79393703a53e2649abf
John Schember <john@nachtimwald.com>
2019-08-08 11:33:31 -0400

    http simple read: Don't decode form data as part of parsing.

--- formats/http/m_http_simple_reader.c
+++ formats/http/m_http_simple_reader.c
@@ -189,23 +189,10 @@ static M_http_error_t M_http_simple_read_decode_body(M_http_simple_read_t *simpl
 	if (M_str_isempty(simple->http->content_type) && simple->http->codec == M_TEXTCODEC_UNKNOWN)
 		return M_HTTP_ERROR_SUCCESS;
 
-	/* If the Content-Type is form encoded we need to decode. */
-	if (M_str_caseeq(simple->http->content_type, "application/x-www-form-urlencoded")) {
-		dec  = NULL;
-		terr = M_textcodec_decode(&dec, M_buf_peek(simple->http->body), M_TEXTCODEC_EHANDLER_REPLACE, M_TEXTCODEC_PERCENT_FORM);
-		M_buf_truncate(simple->http->body, 0);
-		M_buf_add_str(simple->http->body, dec);
-		M_free(dec);
-
-		if (terr != M_TEXTCODEC_ERROR_SUCCESS && terr != M_TEXTCODEC_ERROR_SUCCESS_EHANDLER) {
-			return M_HTTP_ERROR_TEXTCODEC_FAILURE;
-		}
-
-		update_clen = M_TRUE;
-	}
-
 	/* Decode the data to utf-8 if we can. */
-	if (simple->http->codec != M_TEXTCODEC_UNKNOWN && simple->http->codec != M_TEXTCODEC_UTF8) {
+	if (!M_str_caseeq(simple->http->content_type, "application/x-www-form-urlencoded") &&
+			simple->http->codec != M_TEXTCODEC_UNKNOWN && simple->http->codec != M_TEXTCODEC_UTF8)
+	{
 		dec  = NULL;
 		terr = M_textcodec_decode(&dec, M_buf_peek(simple->http->body), M_TEXTCODEC_EHANDLER_REPLACE, simple->http->codec);
 		M_buf_truncate(simple->http->body, 0);
