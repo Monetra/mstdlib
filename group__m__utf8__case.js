e33a5403c9de91e93c9f82a0231698a8f9b01291
Travis CI <>
2019-10-10 20:38:49 +0000

    Deploy to GitHub Pages: 1557ff1289a46f630606c11f1299fec59798ba5d

--- group__m__utf8__case.js
+++ group__m__utf8__case.js
@@ -1,13 +1,13 @@
 var group__m__utf8__case =
 [
-    [ "M_utf8_get_chr_toupper", "group__m__utf8__case.html#ga6c4ab25410df67fff4e69bca4ec7718e", null ],
-    [ "M_utf8_get_chr_toupper_buf", "group__m__utf8__case.html#gaa543c8c576f6da208097327004fd05ba", null ],
-    [ "M_utf8_cp_toupper", "group__m__utf8__case.html#gad2c8b43f5c2ca923d10677b3eeeebc24", null ],
+    [ "M_utf8_toupper_cp", "group__m__utf8__case.html#ga547c0f75614dc29ca9729b0429e2cb5b", null ],
+    [ "M_utf8_toupper_chr", "group__m__utf8__case.html#gae8c51b6c4c69253c57095c5d05362755", null ],
+    [ "M_utf8_toupper_chr_buf", "group__m__utf8__case.html#ga1add1971526c2c982aaa9e31ff6ae687", null ],
     [ "M_utf8_toupper", "group__m__utf8__case.html#ga83a67f82d60be894990f5adf461ced09", null ],
     [ "M_utf8_toupper_buf", "group__m__utf8__case.html#ga6e3a566617f17d7a8d0836393c4103b8", null ],
-    [ "M_utf8_get_chr_tolower", "group__m__utf8__case.html#gac6156c81decb8cfe4f2ed35d5e274f09", null ],
-    [ "M_utf8_get_chr_tolower_buf", "group__m__utf8__case.html#ga45b114542bf2a42cc460c756bd0dd869", null ],
-    [ "M_utf8_cp_tolower", "group__m__utf8__case.html#gaaa503bd63d46182bb754d7b1906bf426", null ],
+    [ "M_utf8_tolower_cp", "group__m__utf8__case.html#ga13d992d9843c5ef0ce90e24d5ad41c8a", null ],
+    [ "M_utf8_tolower_chr", "group__m__utf8__case.html#ga10a0c2ba47ca694d781dca75084d6914", null ],
+    [ "M_utf8_tolower_chr_buf", "group__m__utf8__case.html#gad29ba3a7f46ae907b00c9a8271bf4c9d", null ],
     [ "M_utf8_tolower", "group__m__utf8__case.html#ga0c34725bca23b9b5bd6f696ebdb42df6", null ],
     [ "M_utf8_tolower_buf", "group__m__utf8__case.html#gae13ac5e95c0343f0d0d7a1934b37a816", null ]
 ];
