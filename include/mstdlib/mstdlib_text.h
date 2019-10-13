fbbac66712994e75440e3e1448cfc6ae404cef93
John Schember <john@nachtimwald.com>
2019-10-12 20:26:52 -0400

    Regular expression support.

---
+++ include/mstdlib/mstdlib_text.h
@@ -0,0 +1,14 @@
+#ifndef __MSTDLIB_TEXT_H__
+#define __MSTDLIB_TEXT_H__
+
+/*! \defgroup m_text Text processing
+ *
+ * \code{.c}
+ * #include <mstdlib/mstdlib_text.h>
+ * \endcode
+ * 
+ */
+
+#include <mstdlib/text/m_re.h>
+
+#endif /* __MSTDLIB_TEXT_H__ */
