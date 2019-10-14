181096ce7bcf4e4eb6a310810768d8bcc068e844
Brad House <brad@monetra.com>
2019-10-14 12:46:30 -0400

    tls: remove unused openssl 1.1 compatibility function

--- tls/m_tls.c
+++ tls/m_tls.c
@@ -84,11 +84,6 @@ static BIO_METHOD        *M_tls_bio_method        = NULL;
 
 /* Compatibility functions */
 #if OPENSSL_VERSION_NUMBER < 0x1010000fL || defined(LIBRESSL_VERSION_NUMBER)
-static int SSL_has_pending(const SSL *s)
-{
-	(void)s;
-	return 0;
-}
 
 static void BIO_set_shutdown(BIO *a, int shut)
 {
