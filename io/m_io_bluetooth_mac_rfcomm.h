4fe5ebb4138f46dc8b5ab7835e46ffd60642c9f5
John Schember <john@nachtimwald.com>
2020-02-18 09:16:40 -0500

    io mac bluetooth: rework bluetooth connections for better memory cleanup, prevent a possible crash, allow reusing objects.

--- io/m_io_bluetooth_mac_rfcomm.h
+++ io/m_io_bluetooth_mac_rfcomm.h
@@ -1,17 +1,17 @@
 /* The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2017 Monetra Technologies, LLC.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,14 +29,14 @@
 @interface M_io_bluetooth_mac_rfcomm : NSObject <IOBluetoothRFCOMMChannelDelegate>
 
 /* Initializer */
-+ (id)m_io_bluetooth_mac_rfcomm:(NSString *)mac uuid:(NSString *)uuid handle:(M_io_handle_t *)handle;
++ (id)m_io_bluetooth_mac_rfcomm:(NSString *)mac uuid:(NSString *)uuid;
 
 /* Stardard init/dealloc functions */
-- (id)init:(NSString *)mac uuid:(NSString *)uuid handle:(M_io_handle_t *)handle;
+- (id)init:(NSString *)mac uuid:(NSString *)uuid;
 - (void)dealloc;
 
 /* Start connecting */
-- (BOOL)connect;
+- (void)connect:(M_io_handle_t *)handle;
 
 /* Initiate a close */
 - (void)close;
