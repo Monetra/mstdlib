b2cc9b5c69ed7b352c2e94e19902a396cf5bfe67
Travis CI <>
2020-03-12 21:49:24 +0000

    Deploy to GitHub Pages: 62781bbe152eeab06122883b33932c22e6d8bcd8

--- group__m__email__simple.js
+++ group__m__email__simple.js
@@ -1,6 +1,6 @@
 var group__m__email__simple =
 [
     [ "Email Simple Reader", "group__m__email__simple__read.html", "group__m__email__simple__read" ],
-    [ "Email Simple Reader", "group__m__email__simple__write.html", "group__m__email__simple__write" ],
+    [ "Email Simple Writer", "group__m__email__simple__write.html", "group__m__email__simple__write" ],
     [ "Email Simple Helpers", "group__m__email__simple__helper.html", "group__m__email__simple__helper" ]
 ];
--- search/groups_4.js
+++ search/groups_4.js
@@ -6,7 +6,7 @@ var searchData=
   ['email_20simple',['Email Simple',['../group__m__email__simple.html',1,'']]],
   ['email_20simple_20helpers',['Email Simple Helpers',['../group__m__email__simple__helper.html',1,'']]],
   ['email_20simple_20reader',['Email Simple Reader',['../group__m__email__simple__read.html',1,'']]],
-  ['email_20simple_20reader',['Email Simple Reader',['../group__m__email__simple__write.html',1,'']]],
+  ['email_20simple_20writer',['Email Simple Writer',['../group__m__email__simple__write.html',1,'']]],
   ['endian',['Endian',['../group__m__endian.html',1,'']]],
   ['event_20subsystem',['Event Subsystem',['../group__m__event.html',1,'']]],
   ['event_20based_20i_2fo_20subsystem',['Event Based I/O Subsystem',['../group__m__eventio.html',1,'']]]
