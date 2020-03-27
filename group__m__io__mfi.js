c4748c92b7035ae716651d2f512238603781c0e5
Travis CI <>
2020-03-27 14:25:11 +0000

    Deploy to GitHub Pages: 4db36f42000188a6cec453524940acd459506090

--- group__m__io__mfi.js
+++ group__m__io__mfi.js
@@ -8,7 +8,7 @@ var group__m__io__mfi =
     [ "M_io_mfi_enum_protocol", "group__m__io__mfi.html#ga6e092fb415095219c5f4bab418f9f5f1", null ],
     [ "M_io_mfi_enum_serialnum", "group__m__io__mfi.html#ga63a89a434f9b299b6bb813570deb9e75", null ],
     [ "M_io_mfi_enum_manufacturer", "group__m__io__mfi.html#ga0f1a918affd7752179cb6330f7f2caf6", null ],
-    [ "M_io_mfi_enum_model_numer", "group__m__io__mfi.html#gadaea0e992769bba31ffa40ce80dec0ee", null ],
+    [ "M_io_mfi_enum_model_number", "group__m__io__mfi.html#gaa106f1bc3f57b06b8cb86f1d536d628d", null ],
     [ "M_io_mfi_enum_firmware_revision", "group__m__io__mfi.html#ga3716821924877514bd933506acf5e883", null ],
     [ "M_io_mfi_enum_hardware_revision", "group__m__io__mfi.html#ga5e43a27ab4a6a41d2fc34f1b17b2737d", null ],
     [ "M_io_mfi_create", "group__m__io__mfi.html#gabaed38c6f1e3f9f4d64591b850124346", null ]
--- search/functions_0.js
+++ search/functions_0.js
@@ -904,7 +904,7 @@ var searchData=
   ['m_5fio_5fmfi_5fenum_5ffirmware_5frevision',['M_io_mfi_enum_firmware_revision',['../group__m__io__mfi.html#ga3716821924877514bd933506acf5e883',1,'m_io_mfi.h']]],
   ['m_5fio_5fmfi_5fenum_5fhardware_5frevision',['M_io_mfi_enum_hardware_revision',['../group__m__io__mfi.html#ga5e43a27ab4a6a41d2fc34f1b17b2737d',1,'m_io_mfi.h']]],
   ['m_5fio_5fmfi_5fenum_5fmanufacturer',['M_io_mfi_enum_manufacturer',['../group__m__io__mfi.html#ga0f1a918affd7752179cb6330f7f2caf6',1,'m_io_mfi.h']]],
-  ['m_5fio_5fmfi_5fenum_5fmodel_5fnumer',['M_io_mfi_enum_model_numer',['../group__m__io__mfi.html#gadaea0e992769bba31ffa40ce80dec0ee',1,'m_io_mfi.h']]],
+  ['m_5fio_5fmfi_5fenum_5fmodel_5fnumber',['M_io_mfi_enum_model_number',['../group__m__io__mfi.html#gaa106f1bc3f57b06b8cb86f1d536d628d',1,'m_io_mfi.h']]],
   ['m_5fio_5fmfi_5fenum_5fname',['M_io_mfi_enum_name',['../group__m__io__mfi.html#gac2c96e746c5b18011ae1197ea6cca15d',1,'m_io_mfi.h']]],
   ['m_5fio_5fmfi_5fenum_5fprotocol',['M_io_mfi_enum_protocol',['../group__m__io__mfi.html#ga6e092fb415095219c5f4bab418f9f5f1',1,'m_io_mfi.h']]],
   ['m_5fio_5fmfi_5fenum_5fserialnum',['M_io_mfi_enum_serialnum',['../group__m__io__mfi.html#ga63a89a434f9b299b6bb813570deb9e75',1,'m_io_mfi.h']]],
