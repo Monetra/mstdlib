5c7e8255c500b8ecb7daf3643d697224fc2532ab
Brad House <brad@brad-house.com>
2020-03-27 09:21:54 -0400

    sql: fix quoting

--- sql/m_sql_report.c
+++ sql/m_sql_report.c
@@ -1,17 +1,17 @@
 /* The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2017 Monetra Technologies, LLC.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -475,6 +475,8 @@ typedef enum {
 
 static __inline__ M_bool M_sql_report_data_matches(const unsigned char *data, size_t data_len, const unsigned char *srch, size_t srch_len)
 {
+	/* Due to things like \r\n being used, but \r or \n both require encapsulation, we need to actually do the heavy search
+	 * of any character in the search pattern matches.  Hopefully most search patterns are 1 byte.
 	if (srch_len > data_len)
 		return M_FALSE;
 	if (data_len == 0 || srch_len == 0)
@@ -484,6 +486,13 @@ static __inline__ M_bool M_sql_report_data_matches(const unsigned char *data, si
 	if (srch_len == 1)
 		return M_TRUE;
 	return M_mem_eq(data, srch, srch_len);
+	*/
+	size_t i;
+	for (i=0; data_len && i<srch_len; i++) {
+		if (data[0] == srch[i])
+			return M_TRUE;
+	}
+	return M_FALSE;
 }
 
 
