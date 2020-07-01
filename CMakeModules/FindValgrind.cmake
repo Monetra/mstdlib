5958336e7c74eb232f8f26a9d6fd41501142b900
John Schember <john@nachtimwald.com>
2020-07-01 09:04:59 -0400

    Update find check and valgrind to fix warnings.

--- CMakeModules/FindValgrind.cmake
+++ CMakeModules/FindValgrind.cmake
@@ -7,52 +7,52 @@
 #
 #
 # Legacy variable defines (don't use these, use imported targets instead):
-#  VALGRIND_INCLUDE_DIRS, where to find valgrind/memcheck.h, etc.
-#  VALGRIND_PROGRAM, the valgrind executable.
-#  VALGRIND_FOUND, If false, do not try to use valgrind.
+#  Valgrind_INCLUDE_DIRS, where to find valgrind/memcheck.h, etc.
+#  Valgrind_PROGRAM, the valgrind executable.
+#  Valgrind_FOUND, If false, do not try to use valgrind.
 #
 #
 # If you have valgrind installed in a non-standard place, you can define
 # Valgrind_DIR to tell cmake where it is.
 
 # Find paths, set legacy variable names.
-find_path(VALGRIND_INCLUDE_DIRS
+find_path(Valgrind_INCLUDE_DIRS
 	NAMES         memcheck.h
 	PATH_SUFFIXES valgrind
 	PATHS         "${Valgrind_DIR}"
 	              "$ENV{Valgrind_DIR}"
-	              "${VALGRIND_PREFIX}"
+	              "${Valgrind_PREFIX}"
 	              /usr
 	              /usr/local
 )
-mark_as_advanced(FORCE VALGRIND_INCLUDE_DIRS)
+mark_as_advanced(FORCE Valgrind_INCLUDE_DIRS)
 
-find_program(VALGRIND_PROGRAM
+find_program(Valgrind_PROGRAM
 	NAMES valgrind
 	PATHS "${Valgrind_DIR}/bin"
 	      "$ENV{Valgrind_DIR}/bin"
-	      "${VALGRIND_PREFIX}/bin"
+	      "${Valgrind_PREFIX}/bin"
 	      /usr/bin
 	      /usr/local/bin
 )
-mark_as_advanced(FORCE VALGRIND_PROGRAM)
+mark_as_advanced(FORCE Valgrind_PROGRAM)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(VALGRIND DEFAULT_MSG
-	VALGRIND_INCLUDE_DIRS
-	VALGRIND_PROGRAM
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Valgrind DEFAULT_MSG
+	Valgrind_INCLUDE_DIRS
+	Valgrind_PROGRAM
 )
 
 # Create import libraries.
-if (NOT TARGET Valgrind::valgrind AND VALGRIND_FOUND AND VALGRIND_PROGRAM)
+if (NOT TARGET Valgrind::valgrind AND Valgrind_FOUND AND Valgrind_PROGRAM)
 	add_executable(Valgrind::valgrind IMPORTED)
 	set_target_properties(Valgrind::valgrind PROPERTIES
-		IMPORTED_LOCATION "${VALGRIND_PROGRAM}"
+		IMPORTED_LOCATION "${Valgrind_PROGRAM}"
 	)
 endif ()
 
-if (NOT TARGET Valgrind::headers AND VALGRIND_FOUND AND VALGRIND_INCLUDE_DIRS)
+if (NOT TARGET Valgrind::headers AND Valgrind_FOUND AND Valgrind_INCLUDE_DIRS)
 	add_library(Valgrind::headers INTERFACE IMPORTED)
 	set_target_properties(Valgrind::valgrind PROPERTIES
-		INTERFACE_INCLUDE_DIRECTORIES "${VALGRIND_INCLUDE_DIRS}"
+		INTERFACE_INCLUDE_DIRECTORIES "${Valgrind_INCLUDE_DIRS}"
 	)
 endif ()
