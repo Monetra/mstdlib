4e801ec1b610e74cc723aa0fe2f7760edba0465a
bradh352 <brad@brad-house.com>
2021-03-26 11:59:49 -0400

    io: blocking issue for some reason

--- io/m_io_netdns.c
+++ io/m_io_netdns.c
@@ -101,9 +101,12 @@ static M_bool M_io_netdns_process_cb(M_io_layer_t *layer, M_event_type_t *type)
 	if (handle->state == M_IO_NET_STATE_DISCONNECTING && *type == M_EVENT_TYPE_WRITE)
 		return M_TRUE;
 
+	/* For some reason this seems to mess with blocking io.  Disable for now until we understand why */
+#if 0
 	/* Consume any events that should no longer be delivered after a disconnect or error */
 	if (handle->state == M_IO_NET_STATE_DISCONNECTED || handle->state == M_IO_NET_STATE_ERROR || handle->state == M_IO_NET_STATE_INIT)
 		return M_TRUE;
+#endif
 
 	/* Modify internal state */
 	if (*type == M_EVENT_TYPE_DISCONNECTED)
