0707b6e9c4f337e1c7e95af7e5e63b1187d4eb55
bradh352 <brad@brad-house.com>
2021-06-11 14:06:47 -0400

    add test case for a write followed by disconnect to ensure client receives all data

--- test/tls/check_tls.c
+++ test/tls/check_tls.c
@@ -19,7 +19,9 @@ M_uint64 expected_connections;
 M_io_t  *netserver;
 M_thread_mutex_t *debug_lock = NULL;
 
-#define DEBUG 0
+#define SEND_AND_DISCONNECT_SIZE (1024 * 1024)
+
+#define DEBUG 1
 
 #if defined(DEBUG) && DEBUG
 #include <stdarg.h>
@@ -273,8 +275,8 @@ static void net_serverconn_cb(M_event_t *event, M_event_type_t type, M_io_t *com
 #if DEBUG
 static void trace_ssl(void *cb_arg, M_io_trace_type_t type, M_event_type_t event_type, const unsigned char *data, size_t data_len)
 {
-	char       *buf;
 	M_timeval_t tv;
+	char        *buf;
 
 	M_time_gettimeofday(&tv);
 	if (type == M_IO_TRACE_TYPE_EVENT) {
@@ -284,12 +286,14 @@ M_thread_mutex_unlock(debug_lock);
 		return;
 	}
 
+	if (DEBUG > 2) {
 M_thread_mutex_lock(debug_lock);
-	M_printf("%lld.%06lld: TRACE %p: %s\n", tv.tv_sec, tv.tv_usec, cb_arg, (type == M_IO_TRACE_TYPE_READ)?"READ":"WRITE");
-	buf = M_str_hexdump(M_STR_HEXDUMP_DECLEN, 0, NULL, data, data_len);
-	M_printf("%s\n", buf);
+		M_printf("%lld.%06lld: TRACE %p: %s\n", tv.tv_sec, tv.tv_usec, cb_arg, (type == M_IO_TRACE_TYPE_READ)?"READ":"WRITE");
+		buf = M_str_hexdump(M_STR_HEXDUMP_DECLEN, 0, NULL, data, data_len);
+		M_printf("%s\n", buf);
 M_thread_mutex_unlock(debug_lock);
-	M_free(buf);
+		M_free(buf);
+	}
 }
 
 #endif
@@ -572,6 +576,307 @@ END_TEST
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+
+
+
+static void net_serverconn_sad_cb(M_event_t *event, M_event_type_t type, M_io_t *comm, void *data)
+{
+	unsigned char   buf[1024];
+	size_t          mysize;
+	static M_buf_t *wbuf  = NULL;
+	size_t          i;
+
+	(void)event;
+	(void)data;
+
+	event_debug("net serverconn %p event %s triggered", comm, event_type_str(type));
+	switch (type) {
+		case M_EVENT_TYPE_CONNECTED:
+			event_debug("net serverconn Connected %s [%s]:%u:%u, (TLS: %llums %s %s %s)",
+				net_type(M_io_net_get_type(comm)), M_io_net_get_ipaddr(comm), M_io_net_get_port(comm), M_io_net_get_ephemeral_port(comm),
+				M_tls_get_negotiation_time_ms(comm, M_IO_LAYER_FIND_FIRST_ID),
+				tls_protocol_name(M_tls_get_protocol(comm, M_IO_LAYER_FIND_FIRST_ID)),
+				M_tls_get_cipher(comm, M_IO_LAYER_FIND_FIRST_ID),
+				M_tls_get_sessionreused(comm, M_IO_LAYER_FIND_FIRST_ID)?"session reused":"session not reused");
+
+			/* Populate send buffer */
+			wbuf = M_buf_create();
+			for (i=0; i<SEND_AND_DISCONNECT_SIZE; i++) {
+				M_buf_add_byte(wbuf, '0');
+			}
+
+			/* Fallthru */
+		case M_EVENT_TYPE_WRITE:
+			/* Write entire buffer, if empty, issue disconnect */
+			M_io_write_from_buf(comm, wbuf);
+			if (M_buf_len(wbuf) == 0) {
+				M_io_disconnect(comm);
+			}
+			break;
+
+		case M_EVENT_TYPE_READ:
+			M_io_read(comm, buf, sizeof(buf), &mysize);
+			event_debug("net serverconn %p read %zu bytes: %.*s", comm, mysize, (int)mysize, buf);
+			break;
+
+		case M_EVENT_TYPE_DISCONNECTED:
+		case M_EVENT_TYPE_ERROR:
+			M_buf_cancel(wbuf);
+			wbuf = NULL;
+			if (type == M_EVENT_TYPE_ERROR) {
+				char error[256];
+				M_io_get_error_string(comm, error, sizeof(error));
+				event_debug("net serverconn %p ERROR %s", comm, error);
+				M_event_return(event);
+			}
+			event_debug("net serverconn %p Freeing connection", comm);
+			M_io_destroy(comm);
+			break;
+		default:
+			/* Ignore */
+			break;
+	}
+}
+
+static void net_server_sad_cb(M_event_t *event, M_event_type_t type, M_io_t *comm, void *data)
+{
+	M_io_t     *newcomm;
+	(void)data;
+	event_debug("net server %p event %s triggered", comm, event_type_str(type));
+	switch (type) {
+		case M_EVENT_TYPE_ACCEPT:
+			while (M_io_accept(&newcomm, comm) == M_IO_ERROR_SUCCESS) {
+				event_debug("Accepted new connection");
+				M_event_add(M_event_get_pool(event), newcomm, net_serverconn_sad_cb, NULL);
+			}
+			break;
+		default:
+			/* Ignore */
+			break;
+	}
+//	event_debug("%s(): RETURN", __FUNCTION__);
+}
+
+static void net_client_sad_cb(M_event_t *event, M_event_type_t type, M_io_t *comm, void *data)
+{
+	size_t          orig_size;
+	static M_buf_t *rbuf  = NULL;
+
+	(void)event;
+	(void)data;
+
+	event_debug("net sad client %p event %s triggered", comm, event_type_str(type));
+	switch (type) {
+		case M_EVENT_TYPE_CONNECTED:
+			event_debug("net sad client Connected %s [%s]:%u:%u, (TLS: %llums %s %s %s)",
+				net_type(M_io_net_get_type(comm)), M_io_net_get_ipaddr(comm), M_io_net_get_port(comm), M_io_net_get_ephemeral_port(comm),
+				M_tls_get_negotiation_time_ms(comm, M_IO_LAYER_FIND_FIRST_ID),
+				tls_protocol_name(M_tls_get_protocol(comm, M_IO_LAYER_FIND_FIRST_ID)),
+				M_tls_get_cipher(comm, M_IO_LAYER_FIND_FIRST_ID),
+				M_tls_get_sessionreused(comm, M_IO_LAYER_FIND_FIRST_ID)?"session reused":"session not reused");
+			rbuf = M_buf_create();
+			break;
+
+		case M_EVENT_TYPE_WRITE:
+
+			break;
+
+		case M_EVENT_TYPE_READ:
+			orig_size = M_buf_len(rbuf);
+			M_io_read_into_buf(comm, rbuf);
+			event_debug("net sad client %p read %zu bytes", comm, M_buf_len(rbuf) - orig_size);
+			break;
+
+		case M_EVENT_TYPE_DISCONNECTED:
+		case M_EVENT_TYPE_ERROR:
+			if (type == M_EVENT_TYPE_ERROR) {
+				char error[256];
+				M_io_get_error_string(comm, error, sizeof(error));
+				event_debug("net sad client %p ERROR %s", comm, error);
+				M_event_return(event);
+			} else {
+				if (M_buf_len(rbuf) == SEND_AND_DISCONNECT_SIZE) {
+					event_debug("net sad client received FULL data");
+					M_event_done(event);
+				} else {
+					event_debug("net sad client received partial data: %zu of %zu bytes", M_buf_len(rbuf), (size_t)SEND_AND_DISCONNECT_SIZE);
+					M_event_return(event);
+				}
+			}
+			M_buf_cancel(rbuf);
+			rbuf = NULL;
+			event_debug("net sad client %p Freeing connection", comm);
+			M_io_destroy(comm);
+			break;
+		default:
+			/* Ignore */
+			break;
+	}
+}
+
+static M_event_err_t check_tls_sendanddisconnect_test(void)
+{
+	M_event_t          *event = M_event_pool_create(0);
+	//M_event_t        *event = M_event_create(M_EVENT_FLAG_NONE);
+	M_io_t             *netclient;
+	M_event_err_t       err;
+	char               *realcert;
+	char               *realkey;
+	M_tls_x509_t       *x509;
+	M_tls_serverctx_t  *serverctx;
+	M_tls_clientctx_t  *clientctx;
+	M_io_error_t        ioerr;
+	M_uint16            port = (M_uint16)M_rand_range(NULL, 10000, 50000);
+	const char * const hosts[] = { "localhost" };
+	debug_lock                = M_thread_mutex_create(M_THREAD_MUTEXATTR_NONE);
+
+	/* GENERATE CERTIFICATES */
+	event_debug("Generating certificates");
+
+	/* Generate real cert */
+	realkey = M_tls_rsa_generate_key(2048);
+	if (realkey == NULL) {
+		event_debug("failed to generate RSA private key");
+		return M_EVENT_ERR_RETURN;
+	}
+	x509 = M_tls_x509_new(realkey);
+	if (x509 == NULL) {
+		event_debug("failed to generate X509 cert");
+		return M_EVENT_ERR_RETURN;
+	}
+	if (!M_tls_x509_txt_add(x509, M_TLS_X509_TXT_COMMONNAME, "localhost", M_FALSE)) {
+		event_debug("failed to add common name");
+		return M_EVENT_ERR_RETURN;
+	}
+	if (!M_tls_x509_txt_SAN_add(x509, M_TLS_X509_SAN_TYPE_DNS, "localhost", M_TRUE)) {
+		event_debug("failed to add subjectaltname1");
+		return M_EVENT_ERR_RETURN;
+	}
+	if (!M_tls_x509_txt_SAN_add(x509, M_TLS_X509_SAN_TYPE_DNS, "localhost.localdomain", M_TRUE)) {
+		event_debug("failed to add subjectaltname2");
+		return M_EVENT_ERR_RETURN;
+	}
+	if (!M_tls_x509_txt_SAN_add(x509, M_TLS_X509_SAN_TYPE_IP, "127.0.0.1", M_TRUE)) {
+		event_debug("failed to add subjectaltname3");
+		return M_EVENT_ERR_RETURN;
+	}
+	if (!M_tls_x509_txt_SAN_add(x509, M_TLS_X509_SAN_TYPE_IP, "::1", M_TRUE)) {
+		event_debug("failed to add subjectaltname4");
+		return M_EVENT_ERR_RETURN;
+	}
+
+	realcert = M_tls_x509_selfsign(x509, 365 * 24 * 60 * 60 /* 1 year */);
+	if (realcert == NULL) {
+		event_debug("failed to self-sign");
+		return M_EVENT_ERR_RETURN;
+	}
+	M_tls_x509_destroy(x509);
+
+	/* GENERATE CLIENT CTX */
+	clientctx = M_tls_clientctx_create();
+	if (clientctx == NULL) {
+		event_debug("failed to create clientctx");
+		return M_EVENT_ERR_RETURN;
+	}
+
+	if (!M_tls_clientctx_set_default_trust(clientctx)) {
+		event_debug("failed to set default clientctx trust list");
+		/* Non-fatal since we set our own trust.
+		 * return M_EVENT_ERR_RETURN; */
+	}
+
+	if (!M_tls_clientctx_set_trust_cert(clientctx, (const M_uint8 *)realcert, M_str_len(realcert))) {
+		event_debug("failed to set server cert trust");
+		return M_EVENT_ERR_RETURN;
+	}
+
+
+	serverctx = M_tls_serverctx_create((const M_uint8 *)realkey, M_str_len(realkey), (const M_uint8 *)realcert, M_str_len(realcert), NULL, 0);
+	if (serverctx == NULL) {
+		event_debug("failed to create base serverctx");
+		return M_EVENT_ERR_RETURN;
+	}
+
+
+	/* CLEAN UP */
+	M_free(realkey);
+	M_free(realcert);
+
+	event_debug("starting write then disconnect test");
+
+	while ((ioerr = M_io_net_server_create(&netserver, port, NULL, M_IO_NET_ANY)) == M_IO_ERROR_ADDRINUSE) {
+		M_uint16 newport = (M_uint16)M_rand_range(NULL, 10000, 50000);
+		event_debug("Port %d in use, switching to new port %d", (int)port, (int)newport);
+		port             = newport;
+	}
+
+	if (ioerr != M_IO_ERROR_SUCCESS) {
+		event_debug("failed to create net server");
+		return M_EVENT_ERR_RETURN;
+	}
+
+	if (M_io_tls_server_add(netserver, serverctx, NULL) != M_IO_ERROR_SUCCESS) {
+		event_debug("failed to wrap net server with tls");
+		return M_EVENT_ERR_RETURN;
+	}
+
+#if DEBUG && DEBUG > 1
+	M_io_add_trace(netserver, NULL, trace_ssl, netserver, NULL, NULL);
+#endif
+
+	event_debug("listener started");
+	if (!M_event_add(event, netserver, net_server_sad_cb, NULL)) {
+		event_debug("failed to add net server");
+		return M_EVENT_ERR_RETURN;
+	}
+	event_debug("listener added to event");
+
+	if (M_io_net_client_create(&netclient, NULL, hosts[0], port, M_IO_NET_ANY) != M_IO_ERROR_SUCCESS) {
+		event_debug("failed to create net client");
+		return M_EVENT_ERR_RETURN;
+	}
+
+	if (M_io_tls_client_add(netclient, clientctx, NULL /* host auto-grabbed from underlying net */, NULL) != M_IO_ERROR_SUCCESS) {
+		event_debug("failed to wrap net client with tls");
+		return M_EVENT_ERR_RETURN;
+	}
+
+#if DEBUG && DEBUG > 1
+	M_io_add_trace(netclient, NULL, trace_ssl, netclient, NULL, NULL);
+#endif
+
+	if (!M_event_add(event, netclient, net_client_sad_cb, NULL)) {
+		event_debug("failed to add net client");
+		return M_EVENT_ERR_RETURN;
+	}
+
+
+	event_debug("entering loop");
+	err = M_event_loop(event, 10000);
+	event_debug("exited loop");
+	/* Cleanup */
+
+	M_event_destroy(event);
+
+	M_tls_clientctx_destroy(clientctx);
+	M_tls_serverctx_destroy(serverctx);
+	M_thread_mutex_destroy(debug_lock); debug_lock = NULL;
+	M_library_cleanup();
+
+	return err;
+}
+
+
+START_TEST(check_tls_sendanddisconnect)
+{
+	M_event_err_t err = check_tls_sendanddisconnect_test();
+	ck_assert_msg(err == M_EVENT_ERR_DONE, "expected M_EVENT_ERR_DONE got %s", event_err_msg(err));
+}
+END_TEST
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
 static Suite *tls_suite(void)
 {
 	Suite *suite;
@@ -582,6 +887,11 @@ static Suite *tls_suite(void)
 	tc = tcase_create("tls");
 	tcase_set_timeout(tc, 60);
 	tcase_add_test(tc, check_tls);
+	suite_add_tcase(suite, tc);
+
+	tc = tcase_create("tls send and disconnect");
+	tcase_set_timeout(tc, 10);
+	tcase_add_test(tc, check_tls_sendanddisconnect);
 	suite_add_tcase(suite, tc);
 
 	return suite;
