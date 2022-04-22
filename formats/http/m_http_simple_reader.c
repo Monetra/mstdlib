0a4fdd586b614423f4f57cf2eb73a01a260335b9
Andrew Kistler <akistler@monetra.com>
2022-04-22 17:19:06 -0400

    Add logic to M_http_simple_read_codec() to work better
    modified:   m_http_simple_reader.c

--- formats/http/m_http_simple_reader.c
+++ formats/http/m_http_simple_reader.c
@@ -428,8 +428,25 @@ const char *M_http_simple_read_content_type(const M_http_simple_read_t *simple)
 
 M_textcodec_codec_t M_http_simple_read_codec(const M_http_simple_read_t *simple)
 {
+	char *val;
+	char *charset;
+	M_parser_t *parser = NULL;
+
 	if (simple == NULL)
 		return M_TEXTCODEC_UNKNOWN;
+
+	if (simple->http->codec != M_TEXTCODEC_UNKNOWN)
+		return simple->http->codec;
+
+	val = M_http_header(simple->http, "content-type");
+	parser = M_parser_create_const((const unsigned char*)val, M_str_len(val), M_PARSER_FLAG_NONE);
+	if (M_parser_consume_until(parser, "charset=", 8, M_TRUE) != 0) {
+		charset = M_parser_peek(parser);
+		simple->http->codec = M_textcodec_codec_from_str(charset);
+	}
+	M_parser_destroy(parser);
+	M_free(val);
+
 	return simple->http->codec;
 }
 
