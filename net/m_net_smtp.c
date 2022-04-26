5949b36063250130642fd244e7f2bab55f9b4373
Andrew Kistler <akistler@monetra.com>
2022-04-26 18:13:37 -0400

    added status() and add_proc_endpoint()
    modified:   net/m_net_smtp.c

--- net/m_net_smtp.c
+++ net/m_net_smtp.c
@@ -25,11 +25,25 @@
 #include <mstdlib/net/m_net_smtp.h> /* For M_net_smtp_callbacks */
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
 struct M_net_smtp {
 	M_event_t                         *el;
 	struct M_net_smtp_callbacks        cbs;
 	void                              *thunk;
+	M_list_t                          *proc_endpoints;
+	M_list_t                          *tcp_endpoints;
+	size_t                             number_of_endpoints;
+	M_net_smtp_status_t                status;
 };
+
+typedef struct {
+	size_t idx;
+	char *command;
+	M_list_str_t *args;
+	M_hash_dict_t *env;
+	M_uint64 timeout_ms;
+	size_t max_processes;
+} proc_endpoint_t;
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
@@ -48,11 +62,53 @@ M_net_smtp_t *M_net_smtp_create(M_event_t *el, const struct M_net_smtp_callbacks
 	}
 
 	sp->thunk = thunk;
+	sp->status = M_NET_SMTP_STATUS_NOENDPOINTS;
+
+	sp->proc_endpoints = M_list_create(NULL, M_LIST_NONE);
+	sp->tcp_endpoints = M_list_create(NULL, M_LIST_NONE);
 
 	return sp;
 }
 
+M_net_smtp_status_t M_net_smtp_status(const M_net_smtp_t *sp)
+{
+	return sp->status;
+}
+
+M_bool M_net_smtp_add_endpoint_process(
+	M_net_smtp_t        *sp,
+	const char          *command,
+	const M_list_str_t  *args,
+	const M_hash_dict_t *env,
+	M_uint64             timeout_ms,
+	size_t               max_processes
+)
+{
+	proc_endpoint_t *ep = NULL;
+	ep = M_malloc_zero(sizeof(*ep));
+	ep->idx = sp->number_of_endpoints;
+	ep->command = M_strdup(command);
+	ep->args = M_list_str_duplicate(args);
+	ep->env = M_hash_dict_duplicate(env);
+	ep->timeout_ms = timeout_ms;
+	ep->max_processes = max_processes;
+	sp->number_of_endpoints++;
+	return M_TRUE;
+}
+
+static void destroy_proc_endpoint(const proc_endpoint_t *ep)
+{
+	M_free(ep->command);
+	M_list_str_destroy(ep->args);
+	M_hash_dict_destroy(ep->env);
+}
+
 void M_net_smtp_destroy(M_net_smtp_t *sp)
 {
+	for (size_t i = 0; i < M_list_len(sp->proc_endpoints); i++) {
+		destroy_proc_endpoint(M_list_at(sp->proc_endpoints, i));
+	}
+	M_list_destroy(sp->proc_endpoints, M_TRUE);
+	M_list_destroy(sp->tcp_endpoints, M_FALSE);
 	M_free(sp);
 }
