f69af1f60c149e4e68a4bba998925d819ddee2de
Andrew Kistler <akistler@monetra.com>
2022-04-27 11:46:18 -0400

    Finish stubbing out API
    modified:   net/m_net_smtp.c

--- net/m_net_smtp.c
+++ net/m_net_smtp.c
@@ -34,16 +34,47 @@ struct M_net_smtp {
 	M_list_t                          *tcp_endpoints;
 	size_t                             number_of_endpoints;
 	M_net_smtp_status_t                status;
+	M_dns_t                           *tcp_dns;
+	M_tls_clientctx_t                 *tcp_tls_ctx;
+	M_uint64                           tcp_connect_ms;
+	M_uint64                           tcp_stall_ms;
+	M_uint64                           tcp_idle_ms;
+	M_net_smtp_load_balance_t          load_balance_mode;
+	size_t                             number_of_attempts;
+	M_list_str_t                      *internal_queue;
+	M_bool                             is_external_queue_enabled;
+	char *                           (*external_queue_get_cb)(void);
 };
 
 typedef struct {
-	size_t idx;
-	char *command;
-	M_list_str_t *args;
+	size_t         idx;
+	char          *command;
+	M_list_str_t  *args;
 	M_hash_dict_t *env;
-	M_uint64 timeout_ms;
-	size_t max_processes;
+	M_uint64       timeout_ms;
+	size_t         max_processes;
 } proc_endpoint_t;
+
+typedef struct {
+	size_t      idx;
+	char       *address;
+	M_uint16    port;
+	M_bool      connect_tls;
+	char       *username;
+	char       *password;
+	size_t      max_conns;
+} tcp_endpoint_t;
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+static M_bool is_running(M_net_smtp_t *sp)
+{
+	return (
+		sp->status == M_NET_SMTP_STATUS_PROCESSING ||
+		sp->status == M_NET_SMTP_STATUS_IDLE       ||
+		sp->status == M_NET_SMTP_STATUS_STOPPING
+	);
+}
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
@@ -66,13 +97,121 @@ M_net_smtp_t *M_net_smtp_create(M_event_t *el, const struct M_net_smtp_callbacks
 
 	sp->proc_endpoints = M_list_create(NULL, M_LIST_NONE);
 	sp->tcp_endpoints = M_list_create(NULL, M_LIST_NONE);
+	sp->internal_queue = M_list_str_create(M_LIST_STR_NONE);
 
 	return sp;
 }
 
+static void destroy_proc_endpoint(const proc_endpoint_t *ep)
+{
+	M_free(ep->command);
+	M_list_str_destroy(ep->args);
+	M_hash_dict_destroy(ep->env);
+}
+
+static void destroy_tcp_endpoint(const tcp_endpoint_t *ep)
+{
+	M_free(ep->address);
+	M_free(ep->username);
+	M_free(ep->password);
+}
+
+void M_net_smtp_destroy(M_net_smtp_t *sp)
+{
+	if (sp == NULL)
+		return;
+
+	for (size_t i = 0; i < M_list_len(sp->proc_endpoints); i++) {
+		destroy_proc_endpoint(M_list_at(sp->proc_endpoints, i));
+	}
+	M_list_destroy(sp->proc_endpoints, M_TRUE);
+	for (size_t i = 0; i < M_list_len(sp->tcp_endpoints); i++) {
+		destroy_tcp_endpoint(M_list_at(sp->tcp_endpoints, i));
+	}
+	M_list_destroy(sp->tcp_endpoints, M_TRUE);
+	M_tls_clientctx_destroy(sp->tcp_tls_ctx);
+	M_free(sp);
+}
+
+void M_net_smtp_pause(M_net_smtp_t *sp)
+{
+	if (sp == NULL || !is_running(sp))
+		return;
+
+	sp->status = M_NET_SMTP_STATUS_STOPPING;
+	sp->status = M_NET_SMTP_STATUS_STOPPED;
+}
+
+M_bool M_net_smtp_resume(M_net_smtp_t *sp)
+{
+	if (sp == NULL || sp->status != M_NET_SMTP_STATUS_STOPPED)
+		return M_FALSE;
+	sp->status = M_NET_SMTP_STATUS_IDLE;
+	return M_TRUE;
+}
+
 M_net_smtp_status_t M_net_smtp_status(const M_net_smtp_t *sp)
 {
+	if (sp == NULL)
+		return M_NET_SMTP_STATUS_NOENDPOINTS;
 	return sp->status;
+}
+
+void M_net_smtp_setup_tcp(M_net_smtp_t *sp, M_dns_t *dns, M_tls_clientctx_t *ctx)
+{
+
+	if (sp == NULL || is_running(sp))
+		return;
+
+	sp->tcp_dns = dns;
+	if (sp->tcp_tls_ctx != NULL)
+		M_tls_clientctx_destroy(sp->tcp_tls_ctx);
+
+	M_tls_clientctx_upref(ctx);
+	sp->tcp_tls_ctx = ctx;
+}
+
+void M_net_smtp_setup_tcp_timeouts(M_net_smtp_t *sp, M_uint64 connect_ms, M_uint64 stall_ms, M_uint64 idle_ms)
+{
+
+	if (sp == NULL || is_running(sp))
+		return;
+
+	sp->tcp_connect_ms = connect_ms;
+	sp->tcp_stall_ms   = stall_ms;
+	sp->tcp_idle_ms    = idle_ms;
+}
+
+
+M_bool M_net_smtp_add_endpoint_tcp(
+	M_net_smtp_t *sp,
+	const char   *address,
+	M_uint16      port,
+	M_bool        connect_tls,
+	const char   *username,
+	const char   *password,
+	size_t        max_conns
+)
+{
+	tcp_endpoint_t *ep = NULL;
+
+	if (sp == NULL || is_running(sp))
+		return M_FALSE;
+
+	if (sp->tcp_dns == NULL || sp->tcp_tls_ctx == NULL)
+		return M_FALSE;
+
+	ep = M_malloc_zero(sizeof(*ep));
+	ep->idx = sp->number_of_endpoints;
+	ep->address = M_strdup(address);
+	ep->port = port;
+	ep->connect_tls = connect_tls;
+	ep->username = M_strdup(username);
+	ep->password = M_strdup(password);
+	ep->max_conns = max_conns;
+	sp->number_of_endpoints++;
+	sp->status = M_NET_SMTP_STATUS_STOPPED;
+	return M_TRUE;
 }
 
 M_bool M_net_smtp_add_endpoint_process(
@@ -85,6 +224,10 @@ M_bool M_net_smtp_add_endpoint_process(
 )
 {
 	proc_endpoint_t *ep = NULL;
+
+	if (sp == NULL || is_running(sp))
+		return M_FALSE;
+
 	ep = M_malloc_zero(sizeof(*ep));
 	ep->idx = sp->number_of_endpoints;
 	ep->command = M_strdup(command);
@@ -93,22 +236,88 @@ M_bool M_net_smtp_add_endpoint_process(
 	ep->timeout_ms = timeout_ms;
 	ep->max_processes = max_processes;
 	sp->number_of_endpoints++;
+	sp->status = M_NET_SMTP_STATUS_STOPPED;
 	return M_TRUE;
 }
 
-static void destroy_proc_endpoint(const proc_endpoint_t *ep)
+M_bool M_net_smtp_load_balance(M_net_smtp_t *sp, M_net_smtp_load_balance_t mode)
 {
-	M_free(ep->command);
-	M_list_str_destroy(ep->args);
-	M_hash_dict_destroy(ep->env);
+	if (sp == NULL || is_running(sp))
+		return M_FALSE;
+
+	sp->load_balance_mode = mode;
+	return M_TRUE;
 }
 
-void M_net_smtp_destroy(M_net_smtp_t *sp)
+void M_net_smtp_set_num_attempts(M_net_smtp_t *sp, size_t num)
 {
-	for (size_t i = 0; i < M_list_len(sp->proc_endpoints); i++) {
-		destroy_proc_endpoint(M_list_at(sp->proc_endpoints, i));
+	if (sp == NULL || is_running(sp))
+		return;
+	sp->number_of_attempts = num;
+}
+
+M_list_str_t *M_net_smtp_dump_queue(M_net_smtp_t *sp)
+{
+	M_list_str_t *list;
+	char         *msg;
+	size_t        len;
+
+	if (sp == NULL)
+		return NULL;
+
+	list = M_list_str_create(M_LIST_STR_NONE);
+
+	if (sp->is_external_queue_enabled) {
+		while ((msg = sp->external_queue_get_cb()) != NULL) {
+			M_list_str_insert(list, msg);
+		}
+		return list;
 	}
-	M_list_destroy(sp->proc_endpoints, M_TRUE);
-	M_list_destroy(sp->tcp_endpoints, M_FALSE);
-	M_free(sp);
+
+	len = M_list_str_len(sp->internal_queue);
+	for (size_t i = 0; i < len; i++) {
+		msg = M_list_str_take_first(sp->internal_queue);
+		M_list_str_insert(list, msg);
+		M_free(msg);
+	}
+	return list;
+}
+
+M_bool M_net_smtp_queue_smtp(M_net_smtp_t *sp, const M_email_t *e)
+{
+	char   *msg;
+	M_bool  is_success;
+
+	if (sp == NULL || sp->is_external_queue_enabled) 
+		return M_FALSE;
+
+	msg = M_email_simple_write(e);
+	is_success = M_net_smtp_queue_message(sp, msg);
+	M_free(msg);
+
+	return is_success;
+}
+
+
+M_bool M_net_smtp_queue_message(M_net_smtp_t *sp, const char *msg)
+{
+	if (sp == NULL || sp->is_external_queue_enabled)
+		return M_FALSE;
+
+	return M_list_str_insert(sp->internal_queue, msg);
+}
+
+M_bool M_net_smtp_use_external_queue(M_net_smtp_t *sp, char *(*get_cb)(void))
+{
+	if (sp == NULL || M_list_str_len(sp->internal_queue) != 0)
+		return M_FALSE;
+
+	sp->is_external_queue_enabled = M_TRUE;
+	sp->external_queue_get_cb = get_cb;
+	return M_TRUE;
+}
+
+void M_net_smtp_external_queue_have_messages(M_net_smtp_t *sp)
+{
+	sp->status = M_NET_SMTP_STATUS_PROCESSING;
 }
