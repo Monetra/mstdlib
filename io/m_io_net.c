2e09c63f2e8535b8dce75ea9e026dcab17ee352c
Andrew Kistler <akistler@monetra.com>
2022-07-19 15:14:45 -0400

    Only use SO_LINGER on WIN32_
    modified:   io/m_io_net.c

--- io/m_io_net.c
+++ io/m_io_net.c
@@ -488,7 +488,6 @@ static void M_io_net_set_sockopts(M_io_handle_t *handle)
 {
 	int ival;
 	int rv;
-	struct linger so_linger;
 
 	/* Set Nagle, if enable TCP_NODELAY is 0 (off), otherwise it is 1 (on) */
 	ival = (handle->settings.nagle_enable)?0:1;
@@ -518,10 +517,15 @@ static void M_io_net_set_sockopts(M_io_handle_t *handle)
 	 * |         |          | specified in the l_linger member.                              |                |
 	 * |---------|----------|----------------------------------------------------------------|----------------|
 	 */
-	so_linger.l_onoff  = 1;
-	so_linger.l_linger = 0;
-	rv = setsockopt(handle->data.net.sock, SOL_SOCKET, SO_LINGER, (const void *)&so_linger, sizeof(so_linger));
-	(void)rv; /* silence coverity */
+#ifdef _WIN32
+	do {
+		struct linger so_linger;
+		so_linger.l_onoff  = 1;
+		so_linger.l_linger = 0;
+		rv = setsockopt(handle->data.net.sock, SOL_SOCKET, SO_LINGER, (const void *)&so_linger, sizeof(so_linger));
+		(void)rv; /* silence coverity */
+	} while(0);
+#endif
 
 	do {
 		int sndbuf_size;
