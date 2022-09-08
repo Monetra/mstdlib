64540ae60aac7b2ecec05da4cee5127b4182474d
Andrew Kistler <akistler@monetra.com>
2022-09-08 11:45:42 -0400

    Add Daniel haxx Stenberg's http://http:// URL test.  Fix a bug.
    modified:   formats/url/url_parser.c
    modified:   test/formats/check_url.c

--- test/formats/check_url.c
+++ test/formats/check_url.c
@@ -16,6 +16,21 @@ do {\
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+START_TEST(check_http_http)
+{
+	M_url_t *url = M_url_create("http://http:%%2f%%2fhttp:%%2f%%2f@http://http://?http://#http://");
+	ck_assert_msg(url != NULL, "Should have parsed");
+	ck_assert_msg(M_str_eq(url->schema, "http"), "Should have parsed schema");
+	ck_assert_msg(M_str_eq(url->host, "http"), "Should have parsed host");
+	ck_assert_msg(M_str_eq(url->userinfo, "http:%%2f%%2fhttp:%%2f%%2f"), "Should have parsed userinfo");
+	ck_assert_msg(url->port == NULL, "Shouldn't have parsed port");
+	ck_assert_msg(M_str_eq(url->path, "//http://"), "Should have parsed path");
+	ck_assert_msg(M_str_eq(url->query, "http://"), "Should have parsed query");
+	ck_assert_msg(M_str_eq(url->fragment, "http://"), "Should have parsed fragment");
+	M_url_destroy(url);
+}
+END_TEST
+
 START_TEST(check_url)
 {
 	M_url_t *url        = M_url_create("schema://userinfo@host:8080/path?query#fragment");
@@ -43,7 +58,8 @@ int main(void)
 
 	suite = suite_create("url");
 
-	add_test(suite, check_url);
+	//add_test(suite, check_url);
+	add_test(suite, check_http_http);
 
 	sr = srunner_create(suite);
 	if (getenv("CK_LOG_FILE_NAME")==NULL) srunner_set_log(sr, "check_url.log");
